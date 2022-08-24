ad0b26a05ffcdd9c28f1cab0c6b528047e37e461
abhinand1855@gmail.com <abhinandmadhu08@gmail.com>
2022-08-24 12:46:34 +0530

    update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -89,9 +89,6 @@ function validate() {
   var email = document.getElementById("email").value;
   var subject = document.getElementById("subject").value;
   var message = document.getElementById("message").value;
-  // var error_message = document.getElementById("error_message");
-
-  // error_message.style.padding = "10px";
 
   var text;
   var letters = /^[A-Za-z]+$/;
@@ -107,12 +104,12 @@ function validate() {
       alert(text);
       return false;
   }
-  if (subject == null || subject == " ") {
+  if (subject == null || subject.trim() == "") {
       text = "Enter Subject";
       alert(text);
       return false;
   }
-  if (message == null || message =="") {
+  if (message == null || message.trim() =="") {
       text = "Enter a message";
       alert(text);
       return false;
